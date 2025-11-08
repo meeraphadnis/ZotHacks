41c7a960e2866a417107833d5efaa05d574ffaf9
Julianna Alderete <jyaldere@uci.edu>
2025-11-08 14:48:21 -0800

    Debug export default RecipesPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BottomNavBar from "./components/BottomNavBar";
 import HomePage from "./pages/HomePage";
 import CameraPage from "./pages/CameraPage";
-import RecipesPage from "./pages/RecipesPage";
+import {RecipesPage} from "./pages/RecipesPage";
 import ConfirmImagesPage from "./pages/ConfirmImagesPage"
 import ConfirmIngredientsPage from "./pages/ConfirmIngredientsPage";
+import PotentialRecipesPage from "./pages/PotentialRecipesPage";
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/add" element={<CameraPage />} />
           <Route path="/recipes" element={<RecipesPage />} />
-		  <Route path="/confirmimages" element={<ConfirmImagesPage />} />
-		  <Route path="/confirmingredients" element={<ConfirmIngredientsPage/>}/>
+		      <Route path="/confirmimages" element={<ConfirmImagesPage />} />
+		      <Route path="/confirmingredients" element={<ConfirmIngredientsPage/>}/>
+          <Route path="/potentialrecipes" element={<PotentialRecipesPage/>}/>
 		</Routes>
         <BottomNavBar />
       </div>
